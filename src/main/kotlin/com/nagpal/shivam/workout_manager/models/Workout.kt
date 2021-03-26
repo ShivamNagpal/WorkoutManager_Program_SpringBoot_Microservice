b29379333f48eede4efb1f0e87cb3322e9962d9a
Shivam Nagpal <shivamnagpal432@gmail.com>
2021-03-26 22:11:23 +0530

    Add saveSection API

--- src/main/kotlin/com/nagpal/shivam/workout_manager/models/Workout.kt
+++ src/main/kotlin/com/nagpal/shivam/workout_manager/models/Workout.kt
@@ -17,7 +17,6 @@ class Workout() : BaseModel() {
 
     constructor(workoutRequestDto: WorkoutRequestDto) : this() {
         this.name = workoutRequestDto.name
-        // TODO: Handle the exception if workout level is invalid enum
         this.level = WorkoutLevel.valueOf(workoutRequestDto.level!!.toUpperCase())
         this.description = workoutRequestDto.description
         this.equipments = workoutRequestDto.equipments
