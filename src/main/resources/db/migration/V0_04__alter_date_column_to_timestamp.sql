191f1b9f5f66c7d9b404c7df09581caad59c3939
Shivam Nagpal <shivamnagpal432@gmail.com>
2021-03-29 00:47:35 +0530

    Correct the flyway script and add the conversion from timestamp to timestamptz

--- src/main/resources/db/migration/V0_04__alter_date_column_to_timestamp.sql
+++ src/main/resources/db/migration/V0_04__alter_date_column_to_timestamp.sql
@@ -1,11 +1,23 @@
 alter table drill alter column time_created type TIMESTAMP using time_created::TIMESTAMP;
-alter table drill alter column time_last_modified type TIMESTAMP using time_created::TIMESTAMP;
+alter table drill alter column time_last_modified type TIMESTAMP using time_last_modified::TIMESTAMP;
 
 alter table "section" alter column time_created type TIMESTAMP using time_created::TIMESTAMP;
-alter table "section" alter column time_last_modified type TIMESTAMP using time_created::TIMESTAMP;
+alter table "section" alter column time_last_modified type TIMESTAMP using time_last_modified::TIMESTAMP;
 
 alter table section_drill alter column time_created type TIMESTAMP using time_created::TIMESTAMP;
-alter table section_drill alter column time_last_modified type TIMESTAMP using time_created::TIMESTAMP;
+alter table section_drill alter column time_last_modified type TIMESTAMP using time_last_modified::TIMESTAMP;
 
 alter table workout alter column time_created type TIMESTAMP using time_created::TIMESTAMP;
-alter table workout alter column time_last_modified type TIMESTAMP using time_created::TIMESTAMP;
+alter table workout alter column time_last_modified type TIMESTAMP using time_last_modified::TIMESTAMP;
+
+alter table drill alter column time_created type TIMESTAMPTZ using time_created at time zone 'Asia/Kolkata';
+alter table drill alter column time_last_modified type TIMESTAMPTZ using time_last_modified at time zone 'Asia/Kolkata';
+
+alter table "section" alter column time_created type TIMESTAMPTZ using time_created at time zone 'Asia/Kolkata';
+alter table "section" alter column time_last_modified type TIMESTAMPTZ using time_last_modified at time zone 'Asia/Kolkata';
+
+alter table section_drill alter column time_created type TIMESTAMPTZ using time_created at time zone 'Asia/Kolkata';
+alter table section_drill alter column time_last_modified type TIMESTAMPTZ using time_last_modified at time zone 'Asia/Kolkata';
+
+alter table workout alter column time_created type TIMESTAMPTZ using time_created at time zone 'Asia/Kolkata';
+alter table workout alter column time_last_modified type TIMESTAMPTZ using time_last_modified at time zone 'Asia/Kolkata';
