750fdebc19f236da45872d01c5459ecf541f3665
Shivam Nagpal <shivamnagpal432@gmail.com>
2021-07-17 14:37:16 +0530

    Remove UUID from the Program APIs

--- src/main/kotlin/com/nagpal/shivam/workout_manager/dtos/request/StageRequestDto.kt
+++ src/main/kotlin/com/nagpal/shivam/workout_manager/dtos/request/StageRequestDto.kt
@@ -1,10 +1,11 @@
 package com.nagpal.shivam.workout_manager.dtos.request
 
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class StageRequestDto(
-    @field:NotBlank
-    val programId: String?,
+    @field:NotNull
+    val programId: Long?,
     @field:NotBlank
     val name: String?,
     val description: String?,
