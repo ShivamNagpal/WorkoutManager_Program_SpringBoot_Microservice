4f3c839ebfcb6fc17153312093de708159817445
Shivam Nagpal <shivamnagpal432@gmail.com>
2021-07-18 23:19:01 +0530

    Remove UUID column from DB and the BaseModel

--- src/main/kotlin/com/nagpal/shivam/workout_manager/models/BaseModel.kt
+++ src/main/kotlin/com/nagpal/shivam/workout_manager/models/BaseModel.kt
@@ -1,22 +1,16 @@
 package com.nagpal.shivam.workout_manager.models
 
 import org.hibernate.annotations.CreationTimestamp
-import org.hibernate.annotations.Generated
-import org.hibernate.annotations.GenerationTime
 import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDateTime
-import java.util.*
 import javax.persistence.*
 
 @MappedSuperclass
-open class BaseModel {
+class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
 
-    @Generated(value = GenerationTime.INSERT)
-    @Column(columnDefinition = "uuid", insertable = false, updatable = false)
-    val uuid: UUID? = null
     val deleted: Boolean = false
 
     @CreationTimestamp
---
+++ src/main/resources/db/migration/V0_09__remove_uuid_column_from_all_tables.sql
@@ -0,0 +1,7 @@
+ALTER TABLE public."program" DROP COLUMN uuid;
+ALTER TABLE public.stage DROP COLUMN uuid;
+ALTER TABLE public.workout DROP COLUMN uuid;
+ALTER TABLE public.stage_workout DROP COLUMN uuid;
+ALTER TABLE public."section" DROP COLUMN uuid;
+ALTER TABLE public.drill DROP COLUMN uuid;
+ALTER TABLE public.section_drill DROP COLUMN uuid;
